89216e95a39099cff98635be8d8282d60529a33a
Kuba Sobecki <gosustyle@gmail.com>
2022-11-17 16:57:51 +0100

    Make cart button bump after adding a meal to the cart (using useEffect)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
     const cartCtx = useContext(CartContext);
+    const [btnBumpClass, setBtnBumpClass] = useState(false);
 
     const numberOfCartItems = cartCtx.items.reduce(
         (sum, item) => sum + item.amount,
         0
     );
 
+    const btnClasses = `${classes.button} ${btnBumpClass ? classes.bump : ''}`;
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setBtnBumpClass(false);
+        }, 300);
+
+        return () => {
+            setBtnBumpClass(true);
+            clearTimeout(timeout);
+        };
+    }, [numberOfCartItems]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
